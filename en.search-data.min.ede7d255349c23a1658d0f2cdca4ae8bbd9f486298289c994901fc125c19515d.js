89299ab97f97dfdebcd2c624f9f16a88bef3e88c
A <A@A.com>
2023-02-06 21:58:54 +0800

    rebuilding site Mon  6 Feb 2023 21:58:54 CST

---
+++ en.search-data.min.ede7d255349c23a1658d0f2cdca4ae8bbd9f486298289c994901fc125c19515d.js
@@ -0,0 +1,140 @@
+"use strict";(function(){const t={cache:!0};t.doc={id:"id",field:["title","content"],store:["title","href"]};const e=FlexSearch.create("balance",t);window.bookSearchIndex=e,e.add({id:0,href:"/docs/example/",title:"Home",content:` The aims of this society are: To bring together researchers with different areas of expertise in life sciences in Oxford To provide a platform for Chinese life scientists in Oxford to meet, exchange ideas and establish research collaborations We welcome anyone working in life sciences in Oxford to join us!
+Committee Advisors (in surname order): Prof. Zhanfeng Cui, Prof Tao Dong, Dr Shisong Jiang, Dr Jilong Liu Committee Members (in surname order): Dr Leyuan Bao, Ms Elizabeth Huang, Dr Yuan Jenq Lui, Dr Yingjie Wang, Mr Hanlin Zhang `}),e.add({id:1,href:"/docs/recruit/",title:"Jobs 🎉",content:" "}),e.add({id:2,href:"/docs/membership/",title:"Membership",content:`Membership There are two types of membership: full members and associate members.
+Full members are entitled to free food and drinks offered at the monthly seminars and free attendance at any ad hoc events.
+Associate members can only attend ad hoc events if there are seats available, and they will have to buy an unsubsidized ticket for each event.
+The monthly seminars and Friday tea breaks are nevertheless open to the public.
+Registration fee There are different rates for full membership, depending on when you register (and pay the fee) and if you are a student:
+Early bird rate (before 31 July 2014): Staff £15 Student £5 After 31 July 2014 Staff £20 Student £10 To encourage people to join our society, there is no fee for registering as associate members.
+Registration form To become a member of the society, please click here.
+`}),e.add({id:3,href:"/docs/events/",title:"Events",content:` Ongoing event 🎉 Apply here
+Future events Several activities have been organized in the past few months, and there are more to come!
+Regular events
+Monthly seminars Friday tea breaks These tea breaks allow life scientists in Oxford to meet, chat and socialize after a week\u0026rsquo;s hard work. The tea breaks are held at the Weatherall Institute of Molecular Medicine at 5pm every Friday. Ad hoc events
+Industrial visit Past events We intend to organize an industrial visit to one or two pharmaceutical companies in September/ October 2014.
+We invite two life scientists to give presentations about their research work every month. This month, the presenters are Dr Yiyan Zheng (FER is a plausible novel target for enhancing paclitaxel cytotoxicity in ovarian cancer) and Mr Hanlin Zhang (Autophagy and T cell immunity). They will be presenting at the Weatherall Institute of Molecular Medicine at 5:30pm on 26 September 2014.
+So far, ten life scientists have presented their work at the monthly seminars (many thanks for their time and effort!):
+Dr Veronica Chang (Crystallizing a glycoprotein) and Dr Leyuan Bao (VEGF receptor protein-protein interactions and intracellular signalling in human endothelial cells) on 25 April 2014
+Dr Xiaowei Zheng (Structural biology – see to believe; top) and Dr Wei Gan (Genetic screening for the risk of type 2 diabetes in Han Chinese; bottom) on 30 May 2014:
+Dr Shan Gao (Searching for synthetic lethality in cancer; top) and Dr Yingjie Wang (To bind or not to bind – hyaluronan and LYVE-1 signalling in lymphatics; bottom) on on 4 July 2014:
+Dr Joe Zhu (Coalescent trees and population structures; top) and Dr Qianxin Wu (Deciphering microRNA – target regulation using genome engineering approach; bottom) on 25 July 2014: Prof Tao Dong (IFITM3 rs12252-C is associated with disease severity in pandemic influenza A H1N1/09 infected Chinese individuals; top) and Dr Chang Zheng (What can we learn from observational studies?; bottom) on 29 August 2014: Barbecue at Wolfson College on 12 July 2014:
+News 第26届全英华人生命科学学会顺利线上举行 \u0026ndash; 《英国侨报》
+`}),e.add({id:4,href:"/docs/acknowledgements/",title:"Acknowledgements",content:` Acknowledgements We thank the following people for their support to our society:
+Prof. Tao Dong Tao Dong — Nuffield Department of Medicine (ox.ac.uk)
+Program Leader, MRC Human Immunology Unit Founding Director (Oxford), CAMS-Oxford joint International Centre for Translational Immunology Founding Director (Oxford), Chinese Academy of Medical Sciences Oxford Institute (COI) Prof. Shisong Jiang Shisong Jiang — Department of Oncology (ox.ac.uk)
+ASSOCIATE PROFESSOR/ SENIOR INDUSTRIAL FELLOW, Department of Oncology, University of Oxford Dr. Ji-long Liu Ji-Long Liu — Oxford Stem Cell Institute
+Program Leader, MRC Functional Genomics Unit `}),e.add({id:5,href:"/docs/contact/",title:"About us",content:` About us Several members of the committee helped to organize the very successful 19th Annual Meeting of the Oxford Life Science Association in the UK which took place in Oxford in 2013 (for details, please click here). They believed that a permanent platform was needed for life scientists in Oxford to meet regularly and exchange ideas, and therefore established this society.
+Contact information If you have any questions, please feel free to contact us at oxford.lifescience@outlook.com.
+`}),e.add({id:6,href:"/docs/members/",title:"Members",content:` 成员自我介绍 会长 王颖捷
+王颖捷，纽卡斯尔大学硕士/博士，毕业后在伯明翰大学和牛津大学做博士后研究。我目前的研究兴趣是了解遗传性心肌疾病的炎症反应，包括肥厚性和扩张型心肌病（即HCM和DCM）。自2013年来，和志同道合的朋友们一起创办了牛津华人生命科学协会，致力于为在牛津生命科学领域学习工作的学生学者创业者们提供一个交流合作的平台。
+副会长 杜越
+杜越，牛津大学临床医学系WTP博士后，美国\u0026amp;欧洲基因与细胞基因治疗协会成员， ASGCT卓越研究奖得主。研究领域从兽医到体细胞克隆，从基因编辑到肺部疾病的基因疗法开发与临床转化；曾带领团队开发针对免疫力低下人群的首个抗新冠病毒感染的免疫预防。早C晚A，喜看闲书唱闲歌，且及时当勉励。现任OCLSS副会长，在协会中主要服务支持创业与外联板块发展，力求做好传递者与桥梁，把更多的知识与信息通过OCLSS的窗口与大家分享，很高兴你能来。
+副会长/学生主席 李奇点
+大家好，我叫李奇点，目前是牛津大学化学系博四，主要研究镧系元素在生物成像方面的应用。旅英7年，在伯明翰大学完成本科，在帝国理工完成硕士学业。去年在华大基因和广发期货做过实习。学习和工作之余，参加过一些创投大赛。平时喜欢看书、学日语，最近还迷上了用RAP学英文。
+组织部 部长 刘赓
+刘赓，现任学会组织部部长。 牛津大学Oncology的博后，在ORCRB（俗称绿楼）工作。主要研究方向是转录因子E2F-Rb信号通路调控RNA编辑剪切从而影响肿瘤免疫。业余爱好：下厨（黑暗料理），追剧（英剧美剧），运动（徒步旅行）。希望大家多多支持和参与学会的各项活动，也欢迎大家对活动的形式和内容提出各类建议和意见！
+成员 邓司伟
+邓司伟，DPhil Molecular and Cellular Medicine 一年级，来自Wolfson College，部门位于NDORMS Botnar Research Centre，研究胰腺癌干细胞表观遗传机制，主要承担各种生物信息学分析。爱吃爱睡，还爱好制作黑暗料理。
+陈宇扬
+陈宇扬，牛津大学生物学专业读大四，来自New College主要研究哺乳动物基因组中的残留逆转录病毒片段在免疫系统中的作用。工作之外，爱好古典音乐、旅行、阅读、运动。希望能够在组织学会的活动中，认识更多志同道合的朋友。
+朱烨玄
+朱烨玄，在牛津大学 Biomedical Sciences 读大二，来自St Anne’s College。目前正在积极地探索自己的学术兴趣。课余时间爱好看杂书和读古诗。希望能向协会中的前辈们多多学习，也希望大家能多多关注生物科技这个领域。
+秘书处 秘书长 孙天依
+孙天依，牛津大学药理学系博后，研究方向为PdCM（Pnmt+心肌细胞亚群）的特性结构和生理病理功能。我乐观积极，喜欢与人交流，每当和不同学科不同背景的人交流，与不同的人进行思维碰撞，总能让我多一分对世界的认知与喜爱。除此之外，我还喜欢旅行和体验生活，积极参与各个活动。很荣幸可以加入OCLSS，现任秘书长，希望可以通过组织不同类型的研讨会、创业论坛、分享会和讲座，让更多人了解这个组织，从中收获很多。
+成员 郭伊妮
+大家好！我是正在学习背贯口的郭伊妮，在和Trinity一墙之隔的Balliol College读工程。日常追番打游戏，人越菜瘾越大。热爱选秀和Kpop。虽然爱看喜欢的乐队的live，但同时也是深度民乐爱好者，（作为一个只会中阮的菜鸡）深爱着琵琶和杨琴，晚上最喜欢做的事情就是看星星，认星座。很开心能够认识大家，成为协会秘书处的一员！希望接下来的日子可以学到更多的知识，和大家一起共创辉煌！！
+马子奇
+马子奇 (Jackie Ma, M.D., M.Sc)，牛津心血管医学部访问学生。在中山一院完成住院医师阶段，荣获6封患者感谢信，期间兼任团总支副书记。曾参与多个国家级课题，广泛涉及基础、临床研究（心血管、代谢、呼吸）。
+宋怡璇
+宋怡璇, Biochemistry 4th year from St Peter\u0026rsquo;s College, 目前在Pharmacology department Platt Lab 研究Mechanistic similarity between lysosomal storage disease and mycobacteria infection。业余热爱旅行和摄影
+创业部 部长/财政长 张叶川
+牛津生物学工程系博士三年级，研究方向ASL核磁脑部血流量值计算问题。对生物医药方向的创业公司和相关投资很感兴趣
+成员 石柳
+石柳，牛津大学精神科系博士后，iCURE创业项目奖得主。研究方向专注于开发血液检测用于阿尔茨海默症的早期诊断。爱好看书，电影及徒步。现加入创业部，在协会中主要支持创业板块发展，尤其是如何将科研成果进行产业转化，将更多的资源通过OCLSS的平台与大家分享。
+李思琪
+李思琪，医学院肿瘤系博士二年级，不会说方言的安徽合肥人。研究方向是核酸药物在肿瘤中的传递与治疗。对研究成果的转化与早期投资创业依然在学习探索中，希望和OCLSS同时有专业素质和综合能力的小伙伴们一起学习成长。
+钱颀楠
+钱颀楠, 牛津大学生物细胞部门博士生,研究方向为肉瘤细胞疗法研发。 平时喜欢健身打球,旅游电影。通过加入OCLSS创业部, 我希望为创业板块的发展作出自己的贡献,不断通过OCLSS的平台为大家提供更好的创业环境。
+朱亦巅
+朱亦巅正在就读博士二年级并进行疫苗相关的课题研究，感兴趣接触学习医药方面的项目。爱好有搭建hifi系统等。
+外联部 部长 张盛盼
+我是张盛盼，就读于牛津大学病理系，生物医学二年级博士一枚，研究方向是HIV-1在细胞间传播的分子机制及其对免疫缺陷和炎症等系列并发症治疗的应用。工作上与实验佛系爱恨纠缠，生活中对一切新鲜事物充满好奇，喜欢旅行冒险尝试一些刺激运动。很开心能通过OCLSS这个平台与大家合作交流。
+成员 皮力
+皮力，2020 级 Nuffield Department of Medicine 流行病学方向博士生，St Hugh‘s College。帝国理工/华西校友。研究方向是传染病建模，但最近沉迷塞尔达。期待新一年和大家的合作！
+王小凡
+Hello, 我是爱旅行爱音乐爱化学的Emily 王小凡。就读过G5里面的三个学校UCL, IC \u0026amp; OX现在是牛津大学Dphil inorganic Chemistry 二年级在读。研究课题是EPSRC合作项目：镧系金属核磁造影剂在低氧肿瘤成像和治疗上的应用。非常高兴加入OCLSS外联部，希望大家多多参与OSLSS今年的活动！
+陈曦
+你好😊，我是陈曦，航院17级流体力学硕士。目前在牛津生物医学工程专业读博士二年级，st hildas college
+新媒体部 朱敏茵 (Sandy)
+朱敏茵(Sandy), 新媒体部部长。我于2013年在伦敦大学学院（UCL）获得了生理和药理学位。之后，在伦敦玛丽女王大学完成了硕博连读。目前博后第三年，研究方向主要是心脏医学。爱好美食和郊游。
+郑健青
+获肯尼迪奖学金资助于牛津大学DPhil第三年在读，此前获海伦哈姆林奖学金资助MRes毕业于帝国理工。研究领域包括人工智能于生物医学分析、术前诊疗及手术导航的应用。于国际会议及SCI期刊发表相关研究成果十余篇。在科研转化方面有多项专利。爱好唱K，健身，电影。
+赵玉
+我是新媒体部的赵玉。专业是生物医学，研究方向为肿瘤的放化疗，以及免疫治疗。曾经参与过非小细胞肺癌的放疗增敏研究，目前的主要研究方向为恶性胸水的个体化免疫治疗。
+吴姿诺
+吴姿诺，新媒体部成员。目前为牛津大学Biochemistry本科大一学生。爱好不多，练舞十余载，平日也好抱着只小吉他哼哼曲儿。主要负责公众号推文的设计和排版，如果有任何建议与意见请尽管联系哦～
+过往成员介绍 梁竹
+Dphil in Clinical Medicine，exeter college，专注炎症小体在神经退行性疾病中的研究。（他很神秘不愿意透露过多信息，想多了解可以关注我们公众号）。
+陈欣雨
+hello 大家好，我是陈欣雨，担任宣传的职位，主要负责管理公众号和发送推文的工作。日常是刷剧刷电影发呆散步看杂书打羽毛球，最近也开始发展一些户外运动了。喜欢的电影很杂，一个人安静的时候最喜欢看战争片或者纪录片，或者听着歌四处走走。想学会调酒做饭尤克里里，想去冰岛稻城阿那亚，疫情结束后一定要去伦敦看音乐剧。我喜欢尝试不同领域的新事物，如果有什么想分享的可以来找我，我特别乐意去了解。
+杜越
+杜越，爱丁堡大学博士，牛津大学博士后，美国基因治疗和细胞治疗协会（ASGCT）成员，英国囊性纤维肿基因治疗组织（GTC）成员，主要科研方向是基因编辑和治疗现任职于英国牛津大学医学院。主导了多项科研项目，研究成果多次在国际会议展示，SCI学术发表在【 Molecular Therapy 】【 Journal of Pineal Research 】等核心期刊。在科研成果转化方面，带领团队获得华为杯英国帝国理工大学创新大赛最佳路演奖、中国教育部“春晖杯”创业创新大赛优胜奖、南京六合创业创新大赛二等奖等。
+陈劲廷
+大家好呀，我叫陈劲廷，化学学生。平时有空也会研究下做饭和摄影，希望能和学长学姐多学习交流。
+秦岭
+大家好，我叫秦岭 (但我不负责中国南北气候差异，后院也没有野生大熊猫)。Christ Church 学院，牛津 Clinical Medicine 博士第二年在读，研究方向为病毒入侵宿主分子机制。业余爱好一切棋类和球类运动 (篮球除外)，偶尔玩玩魔方。
+`}),e.add({id:7,href:"/docs/partnership/",title:"商业合作",content:"请填写以下商业合作表 "}),e.add({id:8,href:"/docs/events/form/",title:"Form",content:" "}),e.add({id:9,href:"/docs/events/schedule/",title:"Schedule",content:` OPENING SESSION
+08:30 – 08:40 Opening remarks - Prof. Hua Ye (Associate Professor in Engineering Science, Deputy Director of CRMI Technology Centre, University of Oxford)
+08:40 – 08:50 Speech from the special guest - Mr. Sunan Jiang (Minister Counsellor for Science and Technology Affairs, Chinese Embassy in the UK)
+08:50 – 09:00 Welcome speech from the University of Oxford - Prof. Xin Lu (FMedSci, FRS, Professor of Cancer Biology, Director of Ludwig Cancer Research, University of Oxford)
+SESSION 1 - Neuroscience
+09:00 – 09:40 Plenary speech: Computational approach to brain disease
+- Prof. Jianfeng Feng (Professor of Computational Psychiatry, Warwick University and Fudan University)
+09:40 – 10:00 Invited speech 1: Synaptic transmission dynamics underlying hippocampal sharp-wave ripple oscillations in vivo
+- Dr. Jian Gan (Group Leader in UK Dementia Research Institute, University of Edinburgh)
+10:00 – 10:20 Invited speech 2: The impact of biomolecular condensates on local protein synthesis and axonal health
+- Dr. Julie Qiaojin Lin (Sir Henry Wellcome Research Fellow, Department of Clinical Neurosciences, University of Cambridge)
+10:20 – 10:40 Invited speech 3: Novel tau filament fold in corticobasal degeneration, a four-repeat tauopathy
+- Dr. Wenjuan Zhang (Post-doctoral Fellow, MRC Laboratory of Molecular Biology, University of Cambridge)
+10:40 – 11:00 Invited speech 4: Blood Biomarkers for the diagnosis of Alzheimer\u0026rsquo;s disease
+- Dr. Liu Shi (Senior Postdoc Researcher, Department of Physiology, Anatomy and Genetics, University of Oxford)
+11:00 – 11:20 Coffee break
+SESSION 2 - Bioengineering \u0026amp; Biotechnology
+11:20 – 12:00 Plenary speech: Development of LAMP-based COVID-19 rapid testing kit
+- *Prof. Zhanfeng Cui* (FREng, CEng, MIPEM, CSci, FIChemE, David Pollock Professor of Chemical Engineering, Director of CRMI Technology Centre, Founding Director of Oxford Suzhou Centre for Advanced Research, OSCAR)
+12:00 – 12:20 Invited speech 1: Image2Mech: fill the gap between imaging and robotics in surgery
+- *Dr. Xiao-Yun Zhou* (Senior Research Scientist, PAII. Inc.)
+12:20 – 12:40 Invited speech 2: Bisulfite-free, base-resolution, direct sequencing of DNA methylaiton
+- *Dr. Yibin Liu* (Post-doctoral Fellow, Ludwig Cancer Research, University of Oxford)
+12:40 – 13:00 Invited speech 3: INSIGHT (Isothermal NASBA-Sequencing based hIGH-throughput Test): a two-stage COVID-19 testing strategy
+- Dr. Qianxin Wu (Staff Scientist at Wellcome Sanger Institue)
+13:00 – 13:20 Invited speech 4: 3D Printing and Bioinspired-Fabrication towards Replicating Tissue Functions
+- Dr. Yan Yan Shery Huang (Assistant Professor, Department of Engineering, University of Cambridge)
+13:20 – 14:00 Lunch break
+14:00 – 14:30 COVID-19 Panel discussion: What is the impact of coronavirus?
+SESSION 3 - Big Data
+14:30 – 15:10 Plenary speech:Big data and precision medicine: hypes or hopes?
+- Prof. Zhengming Chen (Professor of Epidemiology and Director of China Programmes, University of Oxford)
+15:10 – 15:30 Invited speech 1: Machine learning for large-scale cardiac image analysis
+- Dr. Wenjia Bai (Lecturer in Artificial Intelligence in Medicine, Data Science Institute, Imperial College London)
+15:30 – 15:50 Invited speech 2: Machine learning challenges in pharmacogenomics
+- Dr. Dennis Wang (Senior Lecturer in Genomic Medicine and Bioinformatics, Sheffield Institute of Translational Neuroscience and Department of Computer Science, University of Sheffield)
+15:50 – 16:10 Invited speech 3: Glucose management using Artificial Intelligence
+- Dr. Kezhi (Ken) Li (Lecturer at Institute of Health Informatics, University College London)
+16:10 – 16:30 Coffee break
+SESSION 4 - Infection \u0026amp; Immunity
+16:30 – 16:50 Invited speech 1: Genetics-led immunological research for novel disease mechanisms and drug targets
+- Dr. Liye Chen (Principal Investigator, Nuffield Department of Orthopaedics, Rheumatology and Musculoskeletal Sciences, University of Oxford)
+16:50 – 17:10 Invited speech 2: Structure and distributions of SARS-CoV-2 spike proteins on intact virions
+- Dr. Zunlong Ke (Post-doctoral Fellow, MRC Laboratory of Molecular Biology, University of Cambridge)
+17:10 – 17:30 Invited speech 3: The origins and relatedness structure of mixed infections vary with local prevalence of P. falciparum malaria
+- Dr. Sha Zhu (Senior statistical analyst)
+17:30 – 18:10 Plenary speech: Broad and strong SARS-COV-2 specific memory T cell responses detected in UK patients recovered from COVID-19
+- Prof. Tao Dong (Professor of Immunology, MRC Human Immunology Unit, Founding Director, Oxford, CAMS-Oxford joint International Centre for Translational Immunology, and Chinese Academy of Medical Science Oxford Institute, University of Oxford)
+CLOSING SESSION
+18:10 – 18:30 Award announcing \u0026amp; closing remarks from current CLSS-UK president - Dr. Rongjun Chen (Reader in Biomaterials Engineering, Imperial College London)
+To register this event, please click here `}),e.add({id:10,href:"/docs/example/hidden/",title:"Hidden",content:`This page is hidden in menu Quondam non pater est dignior ille Eurotas Latent te facies Lorem markdownum arma ignoscas vocavit quoque ille texit mandata mentis ultimus, frementes, qui in vel. Hippotades Peleus pennas conscia cuiquam Caeneus quas.
+Pater demittere evincitque reddunt Maxime adhuc pressit huc Danaas quid freta Soror ego Luctus linguam saxa ultroque prior Tatiumque inquit Saepe liquitur subita superata dederat Anius sudor Cum honorum Latona O fallor in sustinui iussorum equidem. Nymphae operi oris alii fronde parens dumque, in auro ait mox ingenti proxima iamdudum maius?
+reality(burnDocking(apache_nanometer), pad.property_data_programming.sectorBrowserPpga(dataMask, 37, recycleRup)); intellectualVaporwareUser += -5 * 4; traceroute_key_upnp /= lag_optical(android.smb(thyristorTftp)); surge_host_golden = mca_compact_device(dual_dpi_opengl, 33, commerce_add_ppc); if (lun_ipv) { verticalExtranet(1, thumbnail_ttl, 3); bar_graphics_jpeg(chipset - sector_xmp_beta); } Fronde cetera dextrae sequens pennis voce muneris Acta cretus diem restet utque; move integer, oscula non inspirat, noctisque scelus! Nantemque in suas vobis quamvis, et labori!
+var runtimeDiskCompiler = home - array_ad_software; if (internic \u0026gt; disk) { emoticonLockCron += 37 + bps - 4; wan_ansi_honeypot.cardGigaflops = artificialStorageCgi; simplex -= downloadAccess; } var volumeHardeningAndroid = pixel + tftp + onProcessorUnmount; sector(memory(firewire + interlaced, wired)); `}),e.add({id:11,href:"/categories/",title:"Categories",content:""}),e.add({id:12,href:"/docs/",title:"Docs",content:""}),e.add({id:13,href:"/",title:"Introduction",content:` The aims of this society are:
+To bring together researchers with different areas of expertise in life sciences in Oxford To provide a platform for life scientists in Oxford to meet, exchange ideas and establish research collaborations We welcome anyone working in life sciences in Oxford to join us!
+Committee Advisors (in surname order): Prof. Zhanfeng Cui, Prof Tao Dong, Dr Shisong Jiang, Dr Jilong Liu Committee Members (in surname order): Dr Leyuan Bao, Ms Elizabeth Huang, Dr Yuan Jenq Lui, Dr Yingjie Wang, Mr Hanlin Zhang `}),e.add({id:14,href:"/tags/",title:"Tags",content:""})})()
